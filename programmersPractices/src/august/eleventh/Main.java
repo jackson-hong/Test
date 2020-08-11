f593c484c8731c76b29efcc631cb769668c66ea2
Jackson Hong <jacksonhong0111@gmail.com>
2020-08-11 22:39:42 +0900

    8.10 commit

---
+++ programmersPractices/src/august/eleventh/Main.java
@@ -0,0 +1,29 @@
+package august.eleventh;
+
+// https://programmers.co.kr/learn/courses/30/lessons/49993
+
+public class Main {
+	public static void main(String[] args) {
+
+		String[] arr = { "BACDE", "CBADF", "AECB", "BDA" };
+		solution("CBD", arr);
+
+	}
+
+	public static int solution(String skill, String[] skill_trees) {
+		int answer = 0;
+
+//		for (int i = 0; i < skill_trees.length; i++) {
+//			if (skill_trees[i].contains("C")) {
+//				for (int j = 0; j < skill_trees[i].length(); j++) {
+//					
+//				}
+//			}else if (skill_trees[i].contains("D")) {
+//				
+//			}
+//		}
+		
+		
+		return answer;
+	}
+}
