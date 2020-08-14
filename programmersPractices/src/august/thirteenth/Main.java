a6d2a271ddeed832cf50738719082908720eb9db
Jackson Hong <jacksonhong0111@gmail.com>
2020-08-14 11:08:25 +0900

    today's first commit

---
+++ programmersPractices/src/august/thirteenth/Main.java
@@ -0,0 +1,18 @@
+package august.thirteenth;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Main m = new Main();
+		
+	}
+	
+	public int[] solution(int[] answers) {
+        int[] answer = {};
+        int[] pattern1 = {1, 2, 3, 4, 5};
+        int[] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        return answer;
+    }
+}
